127289c94232caafc086c516483f503b957624ed
Mharmotte <141756979+Mharmotte@users.noreply.github.com>
2023-08-15 20:48:46 +0200

    toujours pas

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -6,6 +6,33 @@ namespace SplatoonV5;
 public partial class Pawn : AnimatedEntity
 {
 	[ClientInput] public Vector3 InputDirection { get; set; }
+
+	[ClientInput]
+	public Angles ViewAngles { get; set; }
+	public Vector3 EyePosition
+	{
+		get => Transform.PointToWorld( EyeLocalPosition );
+		set => EyeLocalPosition = Transform.PointToLocal( value );
+	}
+	public Rotation EyeRotation
+	{
+		get => Transform.RotationToWorld( EyeLocalRotation );
+		set => EyeLocalRotation = Transform.RotationToLocal( value );
+	}
+	public Vector3 EyeLocalPosition { get; set; }
+
+	public BBox Hull
+	{
+		get => new
+		(
+			new Vector3( -16, -16, 0 ),
+			new Vector3( 16, 16, 64 )
+		);
+	}
+	[Net, Predicted, Browsable( false )]
+	public Rotation EyeLocalRotation { get; set; }
+	public override Ray AimRay => new Ray( EyePosition, EyeRotation.Forward );
+
 	public float speed = 10f;
 	public override void Spawn()
 	{
@@ -33,6 +60,7 @@ public partial class Pawn : AnimatedEntity
 			Peinture.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
 			Peinture.PhysicsGroup.Velocity = Rotation.Forward * 1000;
 		}
+		EyeLocalPosition = Vector3.Up * (64f * Scale);
 	}
 	public override void FrameSimulate( IClient cl )
 	{
@@ -58,5 +86,21 @@ public partial class Pawn : AnimatedEntity
 	{
 		base.BuildInput();
 		InputDirection = Input.AnalogMove;
+
+		if ( Input.StopProcessing )
+			return;
+
+		var look = Input.AnalogLook;
+
+		if ( ViewAngles.pitch > 90f || ViewAngles.pitch < -90f )
+		{
+			look = look.WithYaw( look.yaw * -1f );
+		}
+
+		var viewAngles = ViewAngles;
+		viewAngles += look;
+		viewAngles.pitch = viewAngles.pitch.Clamp( -89f, 89f );
+		viewAngles.roll = 0f;
+		ViewAngles = viewAngles.Normal;
 	}
 }
