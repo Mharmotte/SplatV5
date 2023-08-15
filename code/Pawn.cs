e961d7bf5856a91eb1f0c2e8d8122d22a6781f7e
Mharmotte <141756979+Mharmotte@users.noreply.github.com>
2023-08-15 20:31:49 +0200

    cameraControl1

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -5,9 +5,8 @@ namespace SplatoonV5;
 
 public partial class Pawn : AnimatedEntity
 {
-	[ClientInput]
-	public Vector3 InputDirection { get; set; }
-	public float speed = 20f;
+	[ClientInput] public Vector3 InputDirection { get; set; }
+	public float speed = 10f;
 	public override void Spawn()
 	{
 		SetModel( "models/citizen/citizen.vmdl" );
@@ -17,12 +16,6 @@ public partial class Pawn : AnimatedEntity
 		EnableShadowInFirstPerson = true;
 	}
 
-	public void DressFromClient( IClient cl )
-	{
-		var c = new ClothingContainer();
-		c.LoadFromClient( cl );
-		c.DressEntity( this );
-	}
 
 	public override void Simulate( IClient cl )
 	{
@@ -41,36 +34,14 @@ public partial class Pawn : AnimatedEntity
 			Peinture.PhysicsGroup.Velocity = Rotation.Forward * 1000;
 		}
 	}
-
+	public override void FrameSimulate( IClient cl )
+	{
+		base.FrameSimulate( cl );
+		Camera.Position = Position + Vector3.Backward * 100;
+	}
 	public override void BuildInput()
 	{
 		base.BuildInput();
 		InputDirection = Input.AnalogMove;
-	}
-
-	bool IsThirdPerson { get; set; } = false;
-
-	public override void FrameSimulate( IClient cl )
-	{
-
-		Camera.Position = Position + Vector3.Backward * 10;
-		Camera.Rotation = Rotation.FromPitch( 90f );
-	}
-
-	public TraceResult TraceBBox( Vector3 start, Vector3 end, Vector3 mins, Vector3 maxs, float liftFeet = 0.0f )
-	{
-		if ( liftFeet > 0 )
-		{
-			start += Vector3.Up * liftFeet;
-			maxs = maxs.WithZ( maxs.z - liftFeet );
-		}
-
-		var tr = Trace.Ray( start, end )
-					.Size( mins, maxs )
-					.WithAnyTags( "solid", "playerclip", "passbullets" )
-					.Ignore( this )
-					.Run();
-
-		return tr;
 	}
 }
